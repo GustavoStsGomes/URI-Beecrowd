276a73ff6e82d7ca74893b6b111a3b5d0c9f5c4b
Gustavo Gomes <gustavo.gomes.95@hotmail.com>
2022-05-03 16:18:10 -0300

    progresso exe1010

--- Beecrowd/src/main/java/beecrowd/Exercicio1010.java
+++ Beecrowd/src/main/java/beecrowd/Exercicio1010.java
@@ -7,24 +7,28 @@ package beecrowd;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Exercicio1006 {
+public class Exercicio1010 {
     public static void main(String[] arg){
         // criando local para armazenar os valores
-        double a, b, c, MP;
+        int cod1, cod2, num1, num2;
+        double valor1, valor2, total;
         
         //trocando localização para EUA para poder usar ponto
         Locale.setDefault(Locale.US);
         
         //realizando a leitura dos valores
         Scanner x = new Scanner(System.in);
-        a = x.nextDouble();
-        b = x.nextDouble();  
-        c = x.nextDouble();
+        cod1 = x.nextInt();
+        num1 = x.nextInt();
+        valor1 = x.nextDouble();
+        cod2 = x.nextInt();
+        num2 = x.nextInt();       
+        valor2 = x.nextDouble();
            
         //realizando a operação
-        MP = ((a * 2) + (b * 3) + (c * 5))/10.0; 
+        total = (num1 * valor1) + (num2 * valor2);
           
         //apresentando resultado
-        System.out.printf("MEDIA = %.1f\n", MP);
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
     }
 }
