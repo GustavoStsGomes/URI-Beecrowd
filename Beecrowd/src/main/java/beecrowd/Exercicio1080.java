b1d38626d28747fe1eab5763a8e372839c9ee709
Gustavo <gustavo.gomes.95@hotmail.com>
2022-06-09 16:46:00 -0300

    updateExercicio1080

--- Beecrowd/src/main/java/beecrowd/Exercicio1080.java
+++ Beecrowd/src/main/java/beecrowd/Exercicio1080.java
@@ -14,17 +14,17 @@ public class Exercicio1080 {
     public static void main (String[] args){
         // criando local para armazenar os valores
         int maiorNumero=0, maiorIndex=0;
-        int[] numeros = new int[5];
+        int[] numeros = new int[100];
         
         //realizando a leitura e alocação dos valores
         Scanner input = new Scanner(System.in);
-        for (int cont = 0; cont < 5; cont++){
+        for (int cont = 0; cont < 100; cont++){
             numeros[cont] = input.nextInt();
         }
         input.close();
         
         //realizando a operação
-        for(int cont = 0; cont < 5; cont ++){
+        for(int cont = 0; cont < 100; cont ++){
             if(numeros[cont] > maiorNumero){
                 maiorNumero = numeros[cont];
                 maiorIndex = cont+1;
