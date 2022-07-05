2f1c41ca33798fc598b1ad715638220cb85849ab
Gustavo <gustavo.gomes.95@hotmail.com>
2022-07-05 15:29:04 -0300

    create1116

---
+++ Beecrowd/src/main/java/beecrowd/Exercicio1116.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio1116 {
+    public static void main (String[] args){
+        // criando local para armazenar os valores
+        int x, y, quantidade;
+        double resultado;
+        
+        //lolizando para poder usar ponto ao invez de virgula
+        Locale.setDefault(Locale.US);
+        
+        //realizando a leitura e alocação dos valores
+        //realizando a operação
+        //apresentando resultado
+        Scanner input = new Scanner(System.in);
+        quantidade = input.nextInt();
+        for (int cont = 0; cont < quantidade; cont++){
+            x = input.nextInt();
+            y = input.nextInt();
+            
+            if(y == 0){
+                System.out.println("divisao impossivel");
+            }else{
+                resultado = (double) x / y; 
+                System.out.printf("%.1f\n", resultado);
+            }
+        }
+        input.close();
+        
+        
+        
+        
+        
+    }
+}
