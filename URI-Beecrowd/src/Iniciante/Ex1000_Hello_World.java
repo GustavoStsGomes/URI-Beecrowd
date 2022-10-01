ab444cfadc91c2730f257eb72580a82e100d68da
Gustavo Gomes <gustavo.gomes.95@hotmail.com>
2022-09-30 22:14:41 -0300

    reBuildExercicio1000

---
+++ URI-Beecrowd/src/Iniciante/Ex1000_Hello_World.java
@@ -0,0 +1,10 @@
+package Iniciante;
+
+public class Ex1000_Hello_World {
+
+    public static void main(String[] args){
+
+        //apresentando resultado
+        System.out.printf("Hello World!\n");
+    }
+}
