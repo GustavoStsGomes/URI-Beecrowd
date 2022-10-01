ba84647f680d04168eee8a6c00eaa204243c9c64
Gustavo Gomes <gustavo.gomes.95@hotmail.com>
2022-09-30 22:16:40 -0300

    createModeloBase

---
+++ URI-Beecrowd/src/modeloBase.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class modeloBase {
+
+    public static void main (String[] args){
+        // criando local para armazenar os valores
+
+        //localizando para poder usar ponto ao invez de virgula
+        Locale.setDefault(Locale.US);
+
+        //realizando a leitura e alocação dos valores
+        Scanner input = new Scanner(System.in);
+        input.close();
+
+        //realizando a operação
+
+        //apresentando resultado
+        //System.out.printf();
+        //System.out.println();
+    }
+
+}
