b49f05172712f4ae92ec74b42363250625163858
Gustavo Gomes <gustavo.gomes.95@hotmail.com>
2022-10-04 16:58:23 -0300

    reUp1010

---
+++ URI-Beecrowd/src/Iniciante/Ex1010_Calculo_Simples.java
@@ -0,0 +1,30 @@
+package Iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex1010_Calculo_Simples {
+    public static void main(String[] arg){
+        // criando local para armazenar os valores
+        int cod1, cod2, num1, num2;
+        double valor1, valor2, total;
+
+        //trocando localização para EUA para poder usar ponto
+        Locale.setDefault(Locale.US);
+
+        //realizando a leitura dos valores
+        Scanner x = new Scanner(System.in);
+        cod1 = x.nextInt();
+        num1 = x.nextInt();
+        valor1 = x.nextDouble();
+        cod2 = x.nextInt();
+        num2 = x.nextInt();
+        valor2 = x.nextDouble();
+
+        //realizando a operação
+        total = (num1 * valor1) + (num2 * valor2);
+
+        //apresentando resultado
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+    }
+}
